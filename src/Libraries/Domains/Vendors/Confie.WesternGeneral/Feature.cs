0eda2a46790fc617fd38e67a57f8a41da223ff72
Michael Matibag <michaelmatibag@gmail.com>
2018-05-15 19:51:36 -0500

    Add Western General domain.

---
+++ src/Libraries/Domains/Vendors/Confie.WesternGeneral/Feature.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Confie.WesternGeneral
+{
+    public class Feature
+    {
+        public string ClaimId { get; set; }
+        public string FeatureId { get; set; }
+        public DateTime OpenDate { get; set; }
+        public DateTime CloseDate { get; set; }
+        public string FeatureStatus { get; set; }
+        public string CoverageCode { get; set; }
+        public string CoverageSubCode { get; set; }
+        public bool ClosedWithoutPayment { get; set; }
+        public decimal Payments { get; set; }
+        public decimal Recoveries { get; set; }
+        public decimal Expenses { get; set; }
+        public decimal ReservesAtBeginning { get; set; }
+        public decimal ReservesAtEnd { get; set; }
+        public decimal TotalIncurredLoss { get; set; }
+    }
+}
---
+++ src/Libraries/Domains/Vendors/Confie.WesternGeneral/PaymentTransactions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Confie.WesternGeneral
+{
+    public class PaymentTransactions
+    {
+        public string ClaimId { get; set; }
+        public string FeatureId { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public decimal PaymentAmount { get; set; }
+        public string PaymentStatus { get; set; }
+        public string PaymentType { get; set; }
+        public string RecoveryType { get; set; }
+        public string CheckNumber { get; set; }
+        public string PaymentAddress { get; set; }
+        public string PaymentCity { get; set; }
+        public string PaymentState { get; set; }
+        public string PaymentZip { get; set; }
+    }
+}
