1d0fae1be355a4623a539dac29c546c0102e7b50
Michael Matibag <michaelmatibag@gmail.com>
2018-05-22 19:16:31 -0500

    ReserveTransactions flat-file specification.

--- src/Libraries/Domains/Vendors/Confie.WesternGeneral/FlatFile/ReserveTransactions.cs
+++ src/Libraries/Domains/Vendors/Confie.WesternGeneral/FlatFile/ReserveTransactions.cs
@@ -1,13 +1,28 @@
 ﻿using System;
+using FileHelpers;
 
 namespace Confie.WesternGeneral.FlatFile
 {
     public class ReserveTransactions
     {
+        [FieldFixedLength(50)]
+        [FieldTrim(TrimMode.Right)]
         public string ClaimId { get; set; }
+
+        [FieldFixedLength(50)]
+        [FieldTrim(TrimMode.Right)]
         public string FeatureId { get; set; }
+
+        [FieldFixedLength(18)]
+        [FieldConverter(ConverterKind.Date)]
         public DateTime ReserveChangeDate { get; set; }
+
+        [FieldFixedLength(17)]
+        [FieldConverter(ConverterKind.Decimal)]
         public decimal ReserveBefore { get; set; }
+
+        [FieldFixedLength(17)]
+        [FieldConverter(ConverterKind.Decimal)]
         public decimal ReserveAfter { get; set; }
     }
 }
