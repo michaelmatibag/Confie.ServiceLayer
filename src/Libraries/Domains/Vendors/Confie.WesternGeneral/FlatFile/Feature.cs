77aaebf01f20e9affe9d54388d432f6aa7f43cf9
Michael Matibag <michaelmatibag@gmail.com>
2018-05-22 19:15:56 -0500

    Feature flat-file specification.

--- src/Libraries/Domains/Vendors/Confie.WesternGeneral/FlatFile/Feature.cs
+++ src/Libraries/Domains/Vendors/Confie.WesternGeneral/FlatFile/Feature.cs
@@ -1,22 +1,64 @@
 ﻿using System;
+using FileHelpers;
 
 namespace Confie.WesternGeneral.FlatFile
 {
     public class Feature
     {
+        [FieldFixedLength(50)]
+        [FieldTrim(TrimMode.Right)]
         public string ClaimId { get; set; }
+
+        [FieldFixedLength(50)]
+        [FieldTrim(TrimMode.Right)]
         public string FeatureId { get; set; }
+
+        [FieldFixedLength(18)]
+        [FieldConverter(ConverterKind.Date)]
         public DateTime OpenDate { get; set; }
+
+        [FieldFixedLength(18)]
+        [FieldConverter(ConverterKind.Date)]
         public DateTime CloseDate { get; set; }
+
+        [FieldFixedLength(30)]
+        [FieldTrim(TrimMode.Right)]
         public string FeatureStatus { get; set; }
+
+        [FieldFixedLength(30)]
+        [FieldTrim(TrimMode.Right)]
         public string CoverageCode { get; set; }
+
+        [FieldFixedLength(30)]
+        [FieldTrim(TrimMode.Right)]
         public string CoverageSubCode { get; set; }
+
+        [FieldFixedLength(1)]
+        [FieldConverter(ConverterKind.Boolean)]
         public bool ClosedWithoutPayment { get; set; }
+
+        [FieldFixedLength(17)]
+        [FieldConverter(ConverterKind.Decimal)]
         public decimal Payments { get; set; }
+
+        [FieldFixedLength(17)]
+        [FieldConverter(ConverterKind.Decimal)]
         public decimal Recoveries { get; set; }
+
+        [FieldFixedLength(17)]
+        [FieldConverter(ConverterKind.Decimal)]
         public decimal Expenses { get; set; }
+
+        [FieldFixedLength(17)]
+        [FieldConverter(ConverterKind.Decimal)]
         public decimal ReservesAtBeginning { get; set; }
+
+        [FieldFixedLength(17)]
+        [FieldConverter(ConverterKind.Decimal)]
         public decimal ReservesAtEnd { get; set; }
+
+        [FieldFixedLength(17)]
+        [FieldConverter(ConverterKind.Decimal)]
         public decimal TotalIncurredLoss { get; set; }
     }
 }
