e079a8032193da704c44e1b04426c66541a88820
Michael Matibag <michaelmatibag@gmail.com>
2018-05-22 19:16:59 -0500

    PaymentTransactions flat-file specifications.

--- src/Libraries/Domains/Vendors/Confie.WesternGeneral/FlatFile/PaymentTransactions.cs
+++ src/Libraries/Domains/Vendors/Confie.WesternGeneral/FlatFile/PaymentTransactions.cs
@@ -1,20 +1,55 @@
 ﻿using System;
+using FileHelpers;
 
 namespace Confie.WesternGeneral.FlatFile
 {
     public class PaymentTransactions
     {
+        [FieldFixedLength(50)]
+        [FieldTrim(TrimMode.Right)]
         public string ClaimId { get; set; }
+
+        [FieldFixedLength(50)]
+        [FieldTrim(TrimMode.Right)]
         public string FeatureId { get; set; }
+
+        [FieldFixedLength(18)]
+        [FieldConverter(ConverterKind.Date)]
         public DateTime PaymentDate { get; set; }
+
+        [FieldFixedLength(17)]
+        [FieldConverter(ConverterKind.Decimal)]
         public decimal PaymentAmount { get; set; }
+
+        [FieldFixedLength(30)]
+        [FieldTrim(TrimMode.Right)]
         public string PaymentStatus { get; set; }
+
+        [FieldFixedLength(30)]
+        [FieldTrim(TrimMode.Right)]
         public string PaymentType { get; set; }
+
+        [FieldFixedLength(50)]
+        [FieldTrim(TrimMode.Right)]
         public string RecoveryType { get; set; }
+
+        [FieldFixedLength(30)]
+        [FieldTrim(TrimMode.Right)]
         public string CheckNumber { get; set; }
+
+        [FieldFixedLength(100)]
+        [FieldTrim(TrimMode.Right)]
         public string PaymentAddress { get; set; }
+
+        [FieldFixedLength(100)]
+        [FieldTrim(TrimMode.Right)]
         public string PaymentCity { get; set; }
+
+        [FieldFixedLength(2)]
         public string PaymentState { get; set; }
+
+        [FieldFixedLength(10)]
+        [FieldTrim(TrimMode.Right)]
         public string PaymentZip { get; set; }
     }
 }
