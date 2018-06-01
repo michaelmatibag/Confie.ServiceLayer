a641f75f48e278c7164ff1e9f7a307a9ee9a8c4e
Michael Matibag <michaelmatibag@gmail.com>
2018-05-31 22:16:28 -0500

    Update flat file classes.

--- src/Libraries/Domains/Vendors/Confie.WesternGeneral/FlatFile/PaymentTransaction.cs
+++ src/Libraries/Domains/Vendors/Confie.WesternGeneral/FlatFile/PaymentTransaction.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using FileHelpers;
+using FileHelpers.Events;
 
 namespace Confie.WesternGeneral.FlatFile
 {
-    public class PaymentTransactions
+    [FixedLengthRecord]
+    public class PaymentTransaction : INotifyRead
     {
         [FieldFixedLength(50)]
         [FieldTrim(TrimMode.Right)]
@@ -14,7 +16,7 @@ namespace Confie.WesternGeneral.FlatFile
         public string FeatureId { get; set; }
 
         [FieldFixedLength(18)]
-        [FieldConverter(ConverterKind.Date)]
+        [FieldConverter(ConverterKind.Date, "yyyy/MM/dd hh:mmtt")]
         public DateTime PaymentDate { get; set; }
 
         [FieldFixedLength(17)]
@@ -51,5 +53,15 @@ namespace Confie.WesternGeneral.FlatFile
         [FieldFixedLength(10)]
         [FieldTrim(TrimMode.Right)]
         public string PaymentZip { get; set; }
+
+        public void BeforeRead(BeforeReadEventArgs e)
+        {
+            e.SkipThisRecord = string.IsNullOrWhiteSpace(e.RecordLine) || e.RecordLine.Equals("\u001a");
+        }
+
+        public void AfterRead(AfterReadEventArgs e)
+        {
+            //Do nothing.
+        }
     }
 }
--- src/Libraries/Domains/Vendors/Confie.WesternGeneral/FlatFile/ReserveTransaction.cs
+++ src/Libraries/Domains/Vendors/Confie.WesternGeneral/FlatFile/ReserveTransaction.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using FileHelpers;
+using FileHelpers.Events;
 
 namespace Confie.WesternGeneral.FlatFile
 {
-    public class ReserveTransactions
+    [FixedLengthRecord]
+    public class ReserveTransaction : INotifyRead
     {
         [FieldFixedLength(50)]
         [FieldTrim(TrimMode.Right)]
@@ -14,7 +16,7 @@ namespace Confie.WesternGeneral.FlatFile
         public string FeatureId { get; set; }
 
         [FieldFixedLength(18)]
-        [FieldConverter(ConverterKind.Date)]
+        [FieldConverter(ConverterKind.Date, "yyyy/MM/dd hh:mmtt")]
         public DateTime ReserveChangeDate { get; set; }
 
         [FieldFixedLength(17)]
@@ -24,5 +26,15 @@ namespace Confie.WesternGeneral.FlatFile
         [FieldFixedLength(17)]
         [FieldConverter(ConverterKind.Decimal)]
         public decimal ReserveAfter { get; set; }
+
+        public void BeforeRead(BeforeReadEventArgs e)
+        {
+            e.SkipThisRecord = string.IsNullOrWhiteSpace(e.RecordLine) || e.RecordLine.Equals("\u001a");
+        }
+
+        public void AfterRead(AfterReadEventArgs e)
+        {
+            //Do nothing.
+        }
     }
 }
