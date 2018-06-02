4e19c28b9e9274863f6e7c0f89cbccd72068acd6
Michael Matibag <michaelmatibag@gmail.com>
2018-06-02 08:29:58 -0500

    Create Claims models.

---
+++ src/Libraries/Domains/Vendors/Confie.WesternGeneral/Claim.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Confie.WesternGeneral
+{
+    public class Claim
+    {
+        public string ClaimId { get; set; }
+        public DateTime LossDate { get; set; }
+        public System.DateTime ReportedDate { get; set; }
+        public DateTime ClosedDate { get; set; }
+        public string ClaimStatus { get; set; }
+        public bool ClosedWithoutPayment { get; set; }
+        public int PercentAtFault { get; set; }
+        public string ClaimDescription { get; set; }
+        public DateTime PolicyEffectiveDate { get; set; }
+        public DateTime PolicyExpirationDate { get; set; }
+        public string PolicyNumber { get; set; }
+        public string LineOfBusiness { get; set; }
+        public string DriverLastName { get; set; }
+        public string DriverFirstName { get; set; }
+        public string DriverLicenseId { get; set; }
+        public string DriverLicenseState { get; set; }
+        public string InsuredVin { get; set; }
+        public string InsuredMake { get; set; }
+        public string InsuredModel { get; set; }
+        public int InsuredYear { get; set; }
+        public decimal Payments { get; set; }
+        public decimal Recoveries { get; set; }
+        public decimal Expenses { get; set; }
+        public decimal ReservesAtBeginning { get; set; }
+        public decimal ReservesAtEnd { get; set; }
+        public decimal TotalIncurredLoss { get; set; }
+        public string SubmissionStatus { get; set; }
+        public string UpdatedUser { get; set; }
+        public string UpdatedDate { get; set; }
+
+        public virtual List<Feature> Features { get; set; }
+
+        public virtual List<PaymentTransaction> PaymentTransactions { get; set; }
+        
+        public virtual List<ReserveTransaction> ReserveTransactions { get; set; }
+    }
+}
---
+++ src/Libraries/Domains/Vendors/Confie.WesternGeneral/Feature.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Confie.WesternGeneral
+{
+    public class Feature
+    {
+        public string FeatureId { get; set; }
+        public DateTime OpenDate { get; set; }
+        public DateTime CloseDate { get; set; }
+        public string FeatureStatus { get; set; }
+        public string CoverageCode { get; set; }
+        public string CoverageSubCode { get; set; }
+        public bool ClosedWithoutPayment { get; set; }
+        public decimal Payments { get; set; }
+        public decimal Recoveries { get; set; }
+        public decimal Expenses { get; set; }
+        public decimal ReservesAtBeginning { get; set; }
+        public decimal ReservesAtEnd { get; set; }
+        public decimal TotalIncurredLoss { get; set; }
+        public string UpdatedUser { get; set; }
+        public string UpdatedDate { get; set; }
+
+        public string ClaimId { get; set; }
+        public virtual Claim Claim { get; set; }
+
+        public virtual List<PaymentTransaction> PaymentTransactions { get; set; }
+
+        public virtual List<ReserveTransaction> ReserveTransactions { get; set; }
+    }
+}
