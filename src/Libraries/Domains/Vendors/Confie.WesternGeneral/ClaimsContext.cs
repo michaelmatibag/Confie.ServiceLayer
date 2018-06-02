07fe242e8c5dcbb1f11f31f2134915fce4d323bb
Michael Matibag <michaelmatibag@gmail.com>
2018-06-02 08:38:21 -0500

    Create ClaimsContext.

---
+++ src/Libraries/Domains/Vendors/Confie.WesternGeneral/ClaimsContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace Confie.WesternGeneral
+{
+    public class ClaimsContext : DbContext
+    {
+        public DbSet<Claim> Claims { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<PaymentTransaction> PaymentTransactions { get; set; }
+        public DbSet<ReserveTransaction> ReserveTransactions { get; set; }
+    }
+}
