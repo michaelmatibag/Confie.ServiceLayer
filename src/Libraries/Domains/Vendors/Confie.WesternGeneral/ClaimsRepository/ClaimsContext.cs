f814f7c622b82d607c796e7912b865b7337d3580
Michael Matibag <michaelmatibag@gmail.com>
2018-06-05 20:30:37 -0500

    Update context for mocking.

--- src/Libraries/Domains/Vendors/Confie.WesternGeneral/ClaimsRepository/ClaimsContext.cs
+++ src/Libraries/Domains/Vendors/Confie.WesternGeneral/ClaimsRepository/ClaimsContext.cs
@@ -4,9 +4,9 @@ namespace Confie.WesternGeneral.ClaimsRepository
 {
     public class ClaimsContext : DbContext
     {
-        public DbSet<Claim> Claims { get; set; }
-        public DbSet<Feature> Features { get; set; }
-        public DbSet<PaymentTransaction> PaymentTransactions { get; set; }
-        public DbSet<ReserveTransaction> ReserveTransactions { get; set; }
+        public virtual IDbSet<Claim> Claims { get; set; }
+        public virtual IDbSet<Feature> Features { get; set; }
+        public virtual IDbSet<PaymentTransaction> PaymentTransactions { get; set; }
+        public virtual IDbSet<ReserveTransaction> ReserveTransactions { get; set; }
     }
 }
