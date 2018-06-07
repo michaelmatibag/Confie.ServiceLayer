7583aa1287313110eeac90eb41ad79f8f95a0337
Michael Matibag <michaelmatibag@gmail.com>
2018-06-06 20:40:11 -0500

    Add Configuration unit test.

---
+++ src/Libraries/Infrastructure/Confie.Infrastructure.Tests/Configuration/ConfigurationTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using Shouldly;
+
+namespace Confie.Infrastructure.Tests.Configuration
+{
+    [TestFixture]
+    public class ConfigurationTests
+    {
+        [Test]
+        public void Configuration_Builds_Configuration()
+        {
+            //Arrange
+            var configuration = new Infrastructure.Configuration.Configuration();
+
+            //Act
+            var result = configuration.BuildFromAppConfiguration<ITestConfiguration>();
+
+            //Assert
+            result.ConfigurationTest.ShouldBe("Configuration.TestValue");
+            result.FileHandlingTest.ShouldBe("FileHandling.TestValue");
+        }
+    }
+}
---
+++ src/Libraries/Infrastructure/Confie.Infrastructure.Tests/Configuration/ITestConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Config.Net;
+
+namespace Confie.Infrastructure.Tests.Configuration
+{
+    public interface ITestConfiguration
+    {
+        [Option(Alias = "Configuration.TestKey")]
+        string ConfigurationTest { get; }
+
+        [Option(Alias = "FileHandling.TestKey")]
+        string FileHandlingTest { get; }
+    }
+}
