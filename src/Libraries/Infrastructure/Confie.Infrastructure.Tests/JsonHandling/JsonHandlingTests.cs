bd7f7d93fdb73f356995d42282e84d411859d189
Michael Matibag <michaelmatibag@gmail.com>
2018-06-09 12:31:45 -0500

    Prepare unit testing.

---
+++ src/Libraries/Infrastructure/Confie.Infrastructure.Tests/JsonHandling/JsonHandlingTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Confie.Infrastructure.Tests.JsonHandling
+{
+    [TestFixture]
+    public class JsonHandlingTests
+    {
+        [Test]
+        public void Temporary()
+        {
+            //TODO:  Add test here.
+        }
+    }
+}
