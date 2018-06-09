4ff73b7a7482ce398fbd3ab620c6291c824dd9c1
Michael Matibag <michaelmatibag@gmail.com>
2018-06-09 16:44:36 -0500

    Add JSON converter.

---
+++ src/Libraries/Infrastructure/Confie.Infrastructure/JsonHandling/Converter.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Confie.Infrastructure.JsonHandling
+{
+    public class Converter : IConverter
+    {
+        public JsonSerializerSettings Settings()
+        {
+            return new JsonSerializerSettings
+            {
+                MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+                DateParseHandling = DateParseHandling.None,
+                Converters =
+                {
+                    new IsoDateTimeConverter
+                    {
+                        DateTimeStyles = DateTimeStyles.AssumeUniversal
+                    }
+                }
+            };
+        }
+    }
+}
