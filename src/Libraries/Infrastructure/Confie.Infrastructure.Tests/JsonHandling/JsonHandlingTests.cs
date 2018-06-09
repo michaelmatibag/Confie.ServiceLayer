b11970504a0754f6fd9ed23f92a74d5b998dd7d0
Michael Matibag <michaelmatibag@gmail.com>
2018-06-09 17:01:41 -0500

    Add FromJson unit test.

--- src/Libraries/Infrastructure/Confie.Infrastructure.Tests/JsonHandling/JsonHandlingTests.cs
+++ src/Libraries/Infrastructure/Confie.Infrastructure.Tests/JsonHandling/JsonHandlingTests.cs
@@ -1,14 +1,44 @@
-﻿using NUnit.Framework;
+﻿using System.IO;
+using Confie.Infrastructure.JsonHandling;
+using Confie.PolicyOne;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Shouldly;
 
 namespace Confie.Infrastructure.Tests.JsonHandling
 {
     [TestFixture]
     public class JsonHandlingTests
     {
-        [Test]
-        public void Temporary()
+        private IConverter _converter;
+
+        [SetUp]
+        public void Setup()
         {
-            //TODO:  Add test here.
+            _converter = MockRepository.GenerateMock<IConverter>();
+        }
+
+        [Test]
+        public void FromJson_Generates_Object()
+        {
+            //Arrange
+            var path = Path.Combine(TestContext.CurrentContext.TestDirectory, "JsonHandling/JsonFiles/PolicyOneClaims.json");
+            var json = File.ReadAllText(path);
+            var jsonHandling = new Infrastructure.JsonHandling.JsonHandling(_converter);
+
+            //Act
+            var result = jsonHandling.FromJson<PolicyOneClaims>(json);
+
+            //Assert
+            result.ShouldBeOfType(typeof(PolicyOneClaims));
+            result.Claims.Count.ShouldBe(1);
+            result.Claims[0].ClaimId.ShouldBe("TESTCLAIMID");
+            result.Claims[0].Features.Count.ShouldBe(1);
+            result.Claims[0].Features[0].ClaimFeatureId.ShouldBe("TESTCLAIMFEATUREID");
+            result.Claims[0].Features[0].Payments.Count.ShouldBe(1);
+            result.Claims[0].Features[0].Payments[0].PaymentId.ShouldBe("TESTPAYMENTID");
+            result.Claims[0].Features[0].Reserves.Count.ShouldBe(1);
+            result.Claims[0].Features[0].Reserves[0].ClaimReserveId.ShouldBe("TESTCLAIMRESERVEID");
         }
     }
 }
