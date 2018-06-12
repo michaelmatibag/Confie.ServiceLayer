352a28f0cbaf0b236d40cc7238107065808b7ccd
Michael Matibag <michaelmatibag@gmail.com>
2018-06-12 13:44:18 -0500

    Update dbcontext of ClaimsRepository.

--- src/Libraries/Domains/Vendors/Confie.WesternGeneral/ClaimsRepository/ClaimsContext.cs
+++ src/Libraries/Domains/Vendors/Confie.WesternGeneral/ClaimsRepository/ClaimsContext.cs
@@ -1,12 +1,36 @@
-﻿using System.Data.Entity;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
 
 namespace Confie.WesternGeneral.ClaimsRepository
 {
     public class ClaimsContext : DbContext
     {
+        public ClaimsContext() : base("Confie.WesternGeneral.ClaimsRepository")
+        {
+        }
+
         public virtual IDbSet<Claim> Claims { get; set; }
         public virtual IDbSet<Feature> Features { get; set; }
         public virtual IDbSet<PaymentTransaction> PaymentTransactions { get; set; }
         public virtual IDbSet<ReserveTransaction> ReserveTransactions { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Claim>()
+                .HasKey(c => c.ClaimId);
+
+            modelBuilder.Entity<Feature>()
+                .HasKey(f => new {f.FeatureId});
+
+            modelBuilder.Entity<PaymentTransaction>()
+                .HasKey(p => p.PaymentTransactionId)
+                .Property(p => p.PaymentTransactionId)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+            modelBuilder.Entity<ReserveTransaction>()
+                .HasKey(r => r.ReserveTransactionId)
+                .Property(r => r.ReserveTransactionId)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+        }
     }
 }
