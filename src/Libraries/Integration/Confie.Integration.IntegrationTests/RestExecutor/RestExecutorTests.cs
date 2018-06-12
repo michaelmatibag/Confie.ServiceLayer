b9bd89ce374d9df24509f22e3b43f73717ba9137
Michael Matibag <michaelmatibag@gmail.com>
2018-06-12 15:32:02 -0500

    Add Integration.IntegrationTests project.

---
+++ src/Libraries/Integration/Confie.Integration.IntegrationTests/RestExecutor/RestExecutorTests.cs
@@ -0,0 +1,36 @@
+﻿using Confie.Infrastructure.Configuration;
+using Confie.Integration.RestExecutor;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Shouldly;
+
+namespace Confie.Integration.IntegrationTests.RestExecutor
+{
+    [TestFixture]
+    public class RestExecutorTests
+    {
+        private IConfiguration _configuration;
+
+        [SetUp]
+        public void Setup()
+        {
+            _configuration = MockRepository.GenerateMock<IConfiguration>();
+        }
+
+        [TestCase("http://loripsum.net/api", "headers", "Lorem ipsum dolor sit amet")]
+        [TestCase("http://jsonplaceholder.typicode.com", "posts/1", "sunt aut facere repellat provident occaecati excepturi optio reprehenderit")]
+        public void Execute_Executes_RestApi(string restApi, string request, string expectedResponse)
+        {
+            //Arrange
+            _configuration.Stub(x => x.BuildFromAppConfiguration<IRestExecutorConfiguration>().RestApi).Return(restApi);
+
+            var restExecutor = new Integration.RestExecutor.RestExecutor(_configuration);
+
+            //Act
+            var result = restExecutor.Execute(request);
+
+            //Assert
+            result.ShouldContain(expectedResponse);
+        }
+    }
+}
