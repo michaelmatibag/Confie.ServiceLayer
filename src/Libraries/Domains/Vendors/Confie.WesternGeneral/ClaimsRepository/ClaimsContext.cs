1ab4e6ae1a0263293e121ff27055830f0e17e2d7
Michael Matibag <michaelmatibag@gmail.com>
2018-06-12 20:45:05 -0500

    Update DB context.

--- src/Libraries/Domains/Vendors/Confie.WesternGeneral/ClaimsRepository/ClaimsContext.cs
+++ src/Libraries/Domains/Vendors/Confie.WesternGeneral/ClaimsRepository/ClaimsContext.cs
@@ -18,19 +18,41 @@ namespace Confie.WesternGeneral.ClaimsRepository
         {
             modelBuilder.Entity<Claim>()
                 .HasKey(c => c.ClaimId);
+            modelBuilder.Entity<Claim>()
+                .Property(c => c.UpdatedUser).IsRequired();
+            modelBuilder.Entity<Claim>()
+                .Property(c => c.UpdatedDate).IsRequired();
 
             modelBuilder.Entity<Feature>()
-                .HasKey(f => new {f.FeatureId});
+                .HasKey(f => f.FeatureId);
+            modelBuilder.Entity<Feature>()
+                .Property(f => f.ClaimId).IsRequired();
+            modelBuilder.Entity<Feature>()
+                .Property(f => f.UpdatedUser).IsRequired();
+            modelBuilder.Entity<Feature>()
+                .Property(f => f.UpdatedDate).IsRequired();
 
             modelBuilder.Entity<PaymentTransaction>()
                 .HasKey(p => p.PaymentTransactionId)
                 .Property(p => p.PaymentTransactionId)
                 .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            modelBuilder.Entity<PaymentTransaction>()
+                .Property(p => p.FeatureId).IsRequired();
+            modelBuilder.Entity<PaymentTransaction>()
+                .Property(p => p.UpdatedUser).IsRequired();
+            modelBuilder.Entity<PaymentTransaction>()
+                .Property(p => p.UpdatedDate).IsRequired();
 
             modelBuilder.Entity<ReserveTransaction>()
                 .HasKey(r => r.ReserveTransactionId)
                 .Property(r => r.ReserveTransactionId)
                 .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            modelBuilder.Entity<ReserveTransaction>()
+                .Property(r => r.FeatureId).IsRequired();
+            modelBuilder.Entity<ReserveTransaction>()
+                .Property(r => r.UpdatedUser).IsRequired();
+            modelBuilder.Entity<ReserveTransaction>()
+                .Property(r => r.UpdatedDate).IsRequired();
         }
     }
 }
