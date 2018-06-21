570ba6e13a11610c83c39ffa176797e7102318a0
Michael Matibag <michaelmatibag@gmail.com>
2018-06-20 21:10:00 -0500

    Add MoneyConverter class.

---
+++ src/Libraries/Infrastructure/Confie.Infrastructure/FileHandling/MoneyConverter.cs
@@ -0,0 +1,14 @@
+﻿using FileHelpers;
+
+namespace Confie.Infrastructure.FileHandling
+{
+    public class MoneyConverter : ConverterBase
+    {
+        public override object StringToField(string from)
+        {
+            decimal.TryParse(from, out var result);
+
+            return result / 100;
+        }
+    }
+}
