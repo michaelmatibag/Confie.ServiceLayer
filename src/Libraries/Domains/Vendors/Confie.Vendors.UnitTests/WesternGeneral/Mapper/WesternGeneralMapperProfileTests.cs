b918123bb2a0f5540eab996ff1ea10749ebca3db
Michael Matibag <michaelmatibag@gmail.com>
2018-06-23 09:22:45 -0500

    Add Western General mapper profile tests.

---
+++ src/Libraries/Domains/Vendors/Confie.Vendors.UnitTests/WesternGeneral/Mapper/WesternGeneralMapperProfileTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Confie.Infrastructure.Mapper;
+using Confie.WesternGeneral.Mapper;
+using NUnit.Framework;
+
+namespace Confie.Vendors.UnitTests.WesternGeneral.Mapper
+{
+    [TestFixture]
+    public class WesternGeneralMapperProfileTests
+    {
+        private Infrastructure.Mapper.Mapper _mapper;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mapper = new Infrastructure.Mapper.Mapper(GetMapperProfiles());
+        }
+
+        [Test]
+        public void Configuration_ShouldBe_Valid()
+        {
+            _mapper.AssertConfigurationIsValid();
+        }
+
+        private static IEnumerable<IMapperProfile> GetMapperProfiles()
+        {
+            yield return new WesternGeneralMapperProfile();
+        }
+    }
+}
