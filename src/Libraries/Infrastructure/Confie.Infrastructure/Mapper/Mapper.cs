333d8cdb903b30d7926d222c9461cdb4a319f74f
Michael Matibag <michaelmatibag@gmail.com>
2018-06-22 20:25:15 -0500

    Implement IMapper.

---
+++ src/Libraries/Infrastructure/Confie.Infrastructure/Mapper/Mapper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using AutoMapper;
+using AutoMapper.Mappers;
+
+namespace Confie.Infrastructure.Mapper
+{
+    public class Mapper : IMapper
+    {
+        private readonly AutoMapper.Configuration _configuration;
+        private readonly IMappingEngine _mappingEngine;
+
+        public Mapper(IEnumerable<IMapperProfile> mapperProfiles)
+        {
+            _configuration = new AutoMapper.Configuration(new TypeMapFactory(), MapperRegistry.AllMappers());
+            _mappingEngine = new MappingEngine(_configuration);
+
+            foreach (var mapperProfile in mapperProfiles)
+            {
+                _configuration.CreateProfile(mapperProfile.GetType().FullName, mapperProfile.Initialize);
+            }
+        }
+
+        public TDestination Map<TSource, TDestination>(TSource source)
+        {
+            return _mappingEngine.Map<TSource, TDestination>(source);
+        }
+    }
+}
