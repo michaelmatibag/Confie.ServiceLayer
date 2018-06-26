0971e93b05f8ace7922713934a093d87a274c169
Michael Matibag <michaelmatibag@gmail.com>
2018-06-26 18:13:59 -0500

    Initial working unit test.

--- src/Libraries/Domains/Vendors/Confie.WesternGeneral/Mapper/WesternGeneralMapperProfile.cs
+++ src/Libraries/Domains/Vendors/Confie.WesternGeneral/Mapper/WesternGeneralMapperProfile.cs
@@ -1,4 +1,5 @@
-﻿using AutoMapper;
+﻿using System;
+using AutoMapper;
 using Confie.Infrastructure.Mapper;
 using Confie.PolicyOne;
 
@@ -8,69 +9,109 @@ namespace Confie.WesternGeneral.Mapper
     {
         public void Initialize(IProfileExpression profileExpression)
         {
-            profileExpression.CreateMap<Claim, IncidentDriver>()
-                .ForMember(d => d.FirstName, s => s.MapFrom(x => x.DriverFirstName))
-                .ForMember(d => d.LastName, s => s.MapFrom(x => x.DriverLastName))
-                .ForMember(d => d.LicenseNumber, s => s.MapFrom(x => x.DriverLicenseId))
-                .ForMember(d => d.LicenseState, s => s.MapFrom(x => x.DriverLicenseState));
-
-            profileExpression.CreateMap<Claim, InsuredDriver>()
-                .ForMember(d => d.LicenseNumber, s => s.MapFrom(x => x.DriverLicenseId))
-                .ForMember(d => d.City, s => s.Ignore())
-                .ForMember(d => d.FirstName, s => s.MapFrom(x => x.DriverFirstName))
-                .ForMember(d => d.LastName, s => s.MapFrom(x => x.DriverLastName))
-                .ForMember(d => d.LicenseState, s => s.MapFrom(x => x.DriverLicenseState))
-                .ForMember(d => d.State, s => s.Ignore())
-                .ForMember(d => d.StreetAddress, s => s.Ignore());
-
-            profileExpression.CreateMap<Claim, NamedInsured>()
-                .ForMember(d => d.State, s => s.Ignore())
-                .ForMember(d => d.City, s => s.Ignore())
-                .ForMember(d => d.FirstName, s => s.MapFrom(x => x.DriverFirstName))
-                .ForMember(d => d.LastName, s => s.MapFrom(x => x.DriverLastName))
-                .ForMember(d => d.StreetAddress, s => s.Ignore());
-
-            profileExpression.CreateMap<Claim, Policy>()
-                .ForMember(d => d.EffectiveDate, s => s.MapFrom(x => x.PolicyEffectiveDate))
-                .ForMember(d => d.ExpirationDate, s => s.MapFrom(x => x.PolicyExpirationDate))
-                .ForMember(d => d.Number, s => s.MapFrom(x => x.PolicyNumber));
-
             profileExpression.CreateMap<Claim, PolicyOne.Claim>()
                 .ForMember(d => d.ClaimId, s => s.MapFrom(x => x.ClaimId))
                 .ForMember(d => d.Number, s => s.MapFrom(x => x.ClaimId))
                 .ForMember(d => d.ImportedTimezoneId, s => s.Ignore())
-                .ForMember(d => d.LossDate, s => s.MapFrom(x => x.LossDate))
+                .ForMember(d => d.LossDate, s => s.MapFrom(x => new DateTimeOffset(x.LossDate)))
                 .ForMember(d => d.ClaimLineOfBusinessType, s => s.MapFrom(x => x.LineOfBusiness))
                 .ForMember(d => d.ClaimSource, s => s.Ignore())
-                .ForMember(d => d.ReportedDate, s => s.MapFrom(x => x.ReportedDate))
-                .ForMember(d => d.ClosedDate, s => s.MapFrom(x => x.ClosedDate))
+                .ForMember(d => d.ReportedDate, s => s.MapFrom(x => new DateTimeOffset(x.ReportedDate)))
+                .ForMember(d => d.ClosedDate, s => s.MapFrom(x => new DateTimeOffset(x.ClosedDate)))
                 .ForMember(d => d.Status, s => s.MapFrom(x => x.ClaimStatus))
                 .ForMember(d => d.Description, s => s.MapFrom(x => x.ClaimDescription))
-                .ForMember(d => d.AccidentType, s => s.Ignore());
+                .ForMember(d => d.AccidentType, s => s.Ignore())
+                .ForMember(d => d.IncidentDriver, s => s.MapFrom(x => new IncidentDriver
+                {
+                    LicenseNumber = x.DriverLicenseId,
+                    FirstName = x.DriverFirstName,
+                    LastName = x.DriverLastName,
+                    LicenseState = x.DriverLicenseState
+                }))
+                .ForMember(d => d.InsuredDriver, s => s.MapFrom(x => new InsuredDriver
+                {
+                    LicenseNumber = x.DriverLicenseId,
+                    State = x.DriverLicenseState,
+                    StreetAddress = string.Empty,
+                    FirstName = x.DriverFirstName,
+                    City = string.Empty,
+                    LastName = x.DriverLastName,
+                    LicenseState = x.DriverLicenseState
+                }))
+                .ForMember(d => d.NamedInsured, s => s.MapFrom(x => new NamedInsured
+                {
+                    State = x.DriverLicenseState,
+                    StreetAddress = string.Empty,
+                    FirstName = x.DriverFirstName,
+                    City = string.Empty,
+                    LastName = x.DriverLastName
+                }))
+                .ForMember(d => d.Policy, s => s.MapFrom(x => new Policy
+                {
+                    Number = x.PolicyNumber,
+                    ExpirationDate = x.PolicyExpirationDate,
+                    EffectiveDate = x.PolicyEffectiveDate
+                }));
 
             profileExpression.CreateMap<Feature, PolicyOne.Feature>()
                 .ForMember(d => d.Number, s => s.MapFrom(x => x.FeatureId))
                 .ForMember(d => d.ClaimFeatureId, s => s.MapFrom(x => x.FeatureId))
-                .ForMember(d => d.ClosedDate, s => s.MapFrom(x => x.CloseDate))
+                .ForMember(d => d.ClosedDate, s => s.MapFrom(x => new DateTimeOffset(x.CloseDate)))
                 .ForMember(d => d.Coverage, s => s.MapFrom(x => x.CoverageCode))
                 .ForMember(d => d.CoverageSubCode, s => s.MapFrom(x => x.CoverageSubCode))
-                .ForMember(d => d.OpenDate, s => s.MapFrom(x => x.OpenDate))
-                .ForMember(d => d.Status, s => s.MapFrom(x => x.FeatureStatus));
+                .ForMember(d => d.OpenDate, s => s.MapFrom(x => new DateTimeOffset(x.OpenDate)))
+                .ForMember(d => d.Status, s => s.MapFrom(x => x.FeatureStatus))
+                .ForMember(d => d.PercentAtFault, s => s.MapFrom(x => x.Claim.PercentAtFault))
+                .ForMember(d => d.LossDescription, s => s.MapFrom(x => x.Claim.ClaimDescription))
+                .ForMember(d => d.Claimant, s => s.MapFrom(x => new Claimant
+                {
+                    Vehicle = new Vehicle
+                    {
+                        Year = x.Claim.InsuredYear,
+                        LicenseNumber = x.Claim.DriverLicenseId,
+                        Model = x.Claim.InsuredModel,
+                        Vin = x.Claim.InsuredVin,
+                        Make = x.Claim.InsuredMake,
+                        LicenseState = x.Claim.DriverLicenseState
+                    },
+                    State = x.Claim.DriverLicenseState,
+                    BusinessName = string.Empty,
+                    FirstName = x.Claim.DriverFirstName,
+                    City = string.Empty,
+                    LastName = x.Claim.DriverLastName
+                }))
+                .ForMember(d => d.InsuredVehicle, s => s.MapFrom(x => new Vehicle
+                {
+                    Year = x.Claim.InsuredYear,
+                    LicenseNumber = x.Claim.DriverLicenseId,
+                    Model = x.Claim.InsuredModel,
+                    Vin = x.Claim.InsuredVin,
+                    Make = x.Claim.InsuredMake,
+                    LicenseState = x.Claim.DriverLicenseState
+                }))
+                .ForMember(d => d.Payments, s => s.MapFrom(x => x.PaymentTransactions))
+                .ForMember(d => d.Reserves, s => s.MapFrom(x => x.ReserveTransactions));
 
-            profileExpression.CreateMap<Claim, Vehicle>()
-                .ForMember(d => d.LicenseNumber, s => s.MapFrom(x => x.DriverLicenseId))
-                .ForMember(d => d.LicenseState, s => s.MapFrom(x => x.DriverLicenseState))
-                .ForMember(d => d.Make, s => s.MapFrom(x => x.InsuredMake))
-                .ForMember(d => d.Model, s => s.MapFrom(x => x.InsuredModel))
-                .ForMember(d => d.Vin, s => s.MapFrom(x => x.InsuredVin))
-                .ForMember(d => d.Year, s => s.MapFrom(x => x.InsuredYear));
-
-            profileExpression.CreateMap<Claim, Claimant>()
+            profileExpression.CreateMap<PaymentTransaction, Payment>()
+                .ForMember(d => d.City, s => s.MapFrom(x => x.PaymentCity))
+                .ForMember(d => d.Address, s => s.MapFrom(x => x.PaymentAddress))
+                .ForMember(d => d.Amount, s => s.MapFrom(x => x.PaymentAmount))
                 .ForMember(d => d.BusinessName, s => s.Ignore())
-                .ForMember(d => d.City, s => s.Ignore())
-                .ForMember(d => d.FirstName, s => s.MapFrom(x => x.DriverFirstName))
-                .ForMember(d => d.LastName, s => s.MapFrom(x => x.DriverLastName))
-                .ForMember(d => d.State, s => s.Ignore());
+                .ForMember(d => d.CheckNumber, s => s.MapFrom(x => x.CheckNumber))
+                .ForMember(d => d.Date, s => s.MapFrom(x => new DateTimeOffset(x.PaymentDate)))
+                .ForMember(d => d.FirstName, s => s.MapFrom(x => x.Claim.DriverFirstName))
+                .ForMember(d => d.LastName, s => s.MapFrom(x => x.Claim.DriverLastName))
+                .ForMember(d => d.PaymentType, s => s.MapFrom(x => x.PaymentType))
+                .ForMember(d => d.RecoveryType, s => s.MapFrom(x => x.RecoveryType))
+                .ForMember(d => d.State, s => s.MapFrom(x => x.PaymentState))
+                .ForMember(d => d.Status, s => s.MapFrom(x => x.PaymentStatus))
+                .ForMember(d => d.Zip, s => s.MapFrom(x => x.PaymentZip))
+                .ForMember(d => d.PaymentId, s => s.MapFrom(x => x.PaymentTransactionId));
+
+            profileExpression.CreateMap<ReserveTransaction, Reserve>()
+                .ForMember(d => d.Amount, s => s.MapFrom(x => x.ReserveAfter))
+                .ForMember(d => d.ChangeDate, s => s.MapFrom(x => new DateTimeOffset(x.ReserveChangeDate)))
+                .ForMember(d => d.ClaimReserveId, s => s.MapFrom(x => x.ReserveTransactionId));
         }
     }
 }
