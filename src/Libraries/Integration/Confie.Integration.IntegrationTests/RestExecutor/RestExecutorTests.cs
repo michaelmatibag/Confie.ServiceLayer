b3b797a2d25754a87b6d8cc2a95cb41710a6d44d
Michael Matibag <michaelmatibag@gmail.com>
2018-06-27 21:57:34 -0500

    Update integration tests.

--- src/Libraries/Integration/Confie.Integration.IntegrationTests/RestExecutor/RestExecutorTests.cs
+++ src/Libraries/Integration/Confie.Integration.IntegrationTests/RestExecutor/RestExecutorTests.cs
@@ -1,4 +1,5 @@
-﻿using Confie.Infrastructure.Configuration;
+﻿using System.Collections.Generic;
+using Confie.Infrastructure.Configuration;
 using Confie.Integration.RestExecutor;
 using NUnit.Framework;
 using Rhino.Mocks;
@@ -19,7 +20,7 @@ namespace Confie.Integration.IntegrationTests.RestExecutor
 
         [TestCase("http://loripsum.net/api", "headers", "Lorem ipsum dolor sit amet")]
         [TestCase("http://jsonplaceholder.typicode.com", "posts/1", "sunt aut facere repellat provident occaecati excepturi optio reprehenderit")]
-        public void Execute_Executes_RestApi(string restApi, string request, string expectedResponse)
+        public void GetString_Executes_RestApi(string restApi, string resource, string expectedResponse)
         {
             //Arrange
             _configuration.Stub(x => x.BuildFromAppConfiguration<IRestExecutorConfiguration>().RestApi).Return(restApi);
@@ -27,10 +28,27 @@ namespace Confie.Integration.IntegrationTests.RestExecutor
             var restExecutor = new Integration.RestExecutor.RestExecutor(_configuration);
 
             //Act
-            var result = restExecutor.Execute(request);
+            var result = restExecutor.Get(resource);
 
             //Assert
             result.ShouldContain(expectedResponse);
+        }
+
+        [Test]
+        public void GetPosts_Executes_RestApi()
+        {
+            //Arrange
+            _configuration.Stub(x => x.BuildFromAppConfiguration<IRestExecutorConfiguration>().RestApi).Return("https://jsonplaceholder.typicode.com");
+
+            var restExecutor = new Integration.RestExecutor.RestExecutor(_configuration);
+
+            //Act
+            var result = restExecutor.Get<List<Post>>("posts");
+
+            //Assert
+            result.ShouldNotBeNull();
+            result.Count.ShouldBe(100);
+            result[0].Title.ShouldBe("sunt aut facere repellat provident occaecati excepturi optio reprehenderit");
         }
     }
 }
