b3efa37312f35e1644db96f4dd6704007e38c6d8
Michael Matibag <michaelmatibag@gmail.com>
2018-06-28 18:34:50 -0500

    Add Post integration tests.

--- src/Libraries/Integration/Confie.Integration.IntegrationTests/RestExecutor/RestExecutorTests.cs
+++ src/Libraries/Integration/Confie.Integration.IntegrationTests/RestExecutor/RestExecutorTests.cs
@@ -50,5 +50,74 @@ namespace Confie.Integration.IntegrationTests.RestExecutor
             result.Count.ShouldBe(100);
             result[0].Title.ShouldBe("sunt aut facere repellat provident occaecati excepturi optio reprehenderit");
         }
+
+        [Test]
+        public void PostPost_Executes_RestApi()
+        {
+            //Arrange
+            _configuration.Stub(x => x.BuildFromAppConfiguration<IRestExecutorConfiguration>().RestApi).Return("https://jsonplaceholder.typicode.com");
+
+            var restExecutor = new Integration.RestExecutor.RestExecutor(_configuration);
+            var request = new Post
+            {
+                Body = "TestBody",
+                Id = 12345,
+                Title = "TestTitle",
+                UserId = 12345
+            };
+
+            //Act
+            var result = restExecutor.Post("posts", request);
+
+            //Assert
+            result.ShouldNotBeNull();
+            result.ShouldBeOfType<Post>();
+            result.Body.ShouldBe("TestBody");
+            result.Id.ShouldBe(12345);
+            result.Title.ShouldBe("TestTitle");
+            result.UserId.ShouldBe(12345);
+        }
+
+        [Test]
+        public void PostPosts_Executes_RestApi()
+        {
+            //Arrange
+            _configuration.Stub(x => x.BuildFromAppConfiguration<IRestExecutorConfiguration>().RestApi).Return("https://jsonplaceholder.typicode.com");
+
+            var restExecutor = new Integration.RestExecutor.RestExecutor(_configuration);
+            var request = new List<Post>
+            {
+                new Post
+                {
+                    Body = "TestBody1",
+                    Id = 11111,
+                    Title = "TestTitle1",
+                    UserId = 11111
+                },
+                new Post
+                {
+                    Body = "TestBody2",
+                    Id = 22222,
+                    Title = "TestTitle2",
+                    UserId = 22222
+                }
+            };
+
+            //Act
+            var result = restExecutor.Post("posts", request);
+
+            //Assert
+            result.ShouldNotBeNull();
+            result.ShouldBeOfType<List<Post>>();
+            result.Count.ShouldBe(2);
+            result[0].Body.ShouldBe("TestBody1");
+            result[0].Id.ShouldBe(11111);
+            result[0].Title.ShouldBe("TestTitle1");
+            result[0].UserId.ShouldBe(11111);
+            result[1].Body.ShouldBe("TestBody2");
+            result[1].Id.ShouldBe(22222);
+            result[1].Title.ShouldBe("TestTitle2");
+            result[1].UserId.ShouldBe(22222);
+        }
     }
 }
