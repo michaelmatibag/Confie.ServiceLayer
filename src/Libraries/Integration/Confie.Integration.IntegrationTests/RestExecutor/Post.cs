709e6fe738506942c1127dbdca98df3051e673d6
Michael Matibag <michaelmatibag@gmail.com>
2018-06-27 21:57:14 -0500

    Add Post test object.

---
+++ src/Libraries/Integration/Confie.Integration.IntegrationTests/RestExecutor/Post.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace Confie.Integration.IntegrationTests.RestExecutor
+{
+    class Post
+    {
+        [JsonProperty("userId")]
+        public long UserId { get; set; }
+
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("body")]
+        public string Body { get; set; }
+    }
+}
