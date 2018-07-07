72111d25f92feea4cd60be38757b35a3fe224848
Michael Matibag <michaelmatibag@gmail.com>
2018-07-07 13:54:03 -0500

    Add dependecy resolver.

---
+++ src/Applications/Framework/Logging/Confie.Logging.WebApi/DependencyResolution/LoggingServiceLogicFeature.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using System.Web.Http;
+using Autofac;
+using Autofac.Integration.WebApi;
+using Confie.Infrastructure.DependencyResolution;
+using LazyCache;
+
+namespace Confie.Logging.WebApi.DependencyResolution
+{
+    public class LoggingServiceLogicFeature
+    {
+        public static void DependencyBuilder()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterAssemblyTypes(AppDomain.CurrentDomain.GetAssemblies())
+                .Where(x => x.GetCustomAttribute<InjectableAttribute>() != null)
+                .AsImplementedInterfaces()
+                .InstancePerRequest();
+            builder.RegisterType<CachingService>()
+                .As<IAppCache>();
+            builder.RegisterWebApiFilterProvider(GlobalConfiguration.Configuration);
+
+            var container = builder.Build();
+            var resolver = new AutofacWebApiDependencyResolver(container);
+
+            GlobalConfiguration.Configuration.DependencyResolver = resolver;
+        }
+    }
+}
