b85b6feee9662113776a0ff82644e77246c22a84
Michael Matibag <michaelmatibag@gmail.com>
2018-07-18 18:09:00 -0500

    Implement File System Repository.

---
+++ src/Libraries/Infrastructure/Confie.Infrastructure/Exceptions/FileOperationException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Confie.Infrastructure.Exceptions
+{
+    public class FileOperationException : Exception
+    {
+        public FileOperationException(string message) : base(message)
+        {
+        }
+    }
+}
