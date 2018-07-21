a2a24d001227695cc50ff0768d89ea3a95036b0c
Michael Matibag <michaelmatibag@gmail.com>
2018-07-21 09:42:38 -0500

    Add initial tests.

--- src/Libraries/Infrastructure/Confie.Infrastructure.UnitTests/FileRepositories/FileSystemRepositoriesTests.cs
+++ src/Libraries/Infrastructure/Confie.Infrastructure.UnitTests/FileRepositories/FileSystemRepositoriesTests.cs
@@ -1,9 +1,63 @@
-﻿using NUnit.Framework;
+﻿using System.IO.Abstractions.TestingHelpers;
+using Confie.Infrastructure.Configuration;
+using Confie.Infrastructure.Exceptions;
+using Confie.Infrastructure.FileRepositories;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Shouldly;
 
 namespace Confie.Infrastructure.UnitTests.FileRepositories
 {
     [TestFixture]
     public class FileSystemRepositoriesTests
     {
+        private MockFileSystem _fileSystem;
+        private IConfigurationRepository _configurationRepository;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fileSystem = new MockFileSystem();
+            _configurationRepository = MockRepository.GenerateStub<IConfigurationRepository>();
+        }
+
+        [Test]
+        public void CopyFile_ThrowsException_IfSourceIsDirectory()
+        {
+            //Arrange
+            const string source = @"C:\source";
+            const string destination = @"C:\destination";
+
+            _fileSystem.AddDirectory(source);
+
+            var fileSystemRepository = GetFileSystemRepository();
+
+            //Act & Assert
+            Should.Throw<FileOperationException>(() => { fileSystemRepository.CopyFile(source, destination); }).Message
+                .ShouldBe($"The source file {source} is a directory and is invalid.");
+        }
+
+        [Test]
+        public void CopyFile_OverwritesFile_OnTrueOverwriteConfiguration()
+        {
+            //Arrange
+            const string source = @"C:\source\test.dat";
+            const string destination = @"C:\destination\test.dat";
+
+            _fileSystem.AddFile(source, MockFileData.NullObject);
+            _fileSystem.AddFile(destination, MockFileData.NullObject);
+            _configurationRepository.Stub(x => x.GetConfigurationValue<bool>("FileSystemRepository.Overwrite")).Return(true);
+
+            var fileSystemRepository = GetFileSystemRepository();
+
+            //Act & Assert
+            Should.NotThrow(() => { fileSystemRepository.CopyFile(source, destination); });
+            _fileSystem.FileExists(destination).ShouldBe(true);
+        }
+
+        private FileSystemRepository GetFileSystemRepository()
+        {
+            return new FileSystemRepository(_fileSystem, _configurationRepository);
+        }
     }
 }
