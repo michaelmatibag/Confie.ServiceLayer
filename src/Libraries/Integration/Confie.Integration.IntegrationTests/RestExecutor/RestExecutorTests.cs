93ae6291ff5db0836adc9fb626fb3b9677e148ab
Michael Matibag <michaelmatibag@gmail.com>
2018-08-06 10:49:18 -0500

    Update Rest Executor unit test.

--- src/Libraries/Integration/Confie.Integration.IntegrationTests/RestExecutor/RestExecutorTests.cs
+++ src/Libraries/Integration/Confie.Integration.IntegrationTests/RestExecutor/RestExecutorTests.cs
@@ -48,7 +48,7 @@ namespace Confie.Integration.IntegrationTests.RestExecutor
 
             //Assert
             result.ShouldNotBeNull();
-            result.Count.ShouldBe(101);
+            result.Count.ShouldBe(100);
             result[0].Title.ShouldBe("sunt aut facere repellat provident occaecati excepturi optio reprehenderit");
         }
 
