91ad4b5819641a681bca36f45a05a7846e306bb9
Michael Matibag <michaelmatibag@gmail.com>
2018-09-05 19:22:36 -0500

    Add initial Run logic with configuration.

---
+++ src/Applications/Vendors/WesternGeneral/Confie.WesternGeneral.ServiceLogic/Applications/ImportClaimsApplication.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Confie.Infrastructure.Application;
+using Confie.Infrastructure.FileHandling;
+using Confie.Infrastructure.FileRepositories;
+using Confie.WesternGeneral.ServiceLogic.Configuration;
+
+namespace Confie.WesternGeneral.ServiceLogic.Applications
+{
+    public class ImportClaimsApplication : IApplication
+    {
+        private readonly ImportClaimsConfiguration _importClaimsConfiguration;
+        private readonly IFileRepository _fileRepository;
+        private readonly IFileHandling _fileHandling;
+
+        public ImportClaimsApplication(ImportClaimsConfiguration importClaimsConfiguration, IFileRepository fileRepository, IFileHandling fileHandling)
+        {
+            _importClaimsConfiguration = importClaimsConfiguration;
+            _fileRepository = fileRepository;
+            _fileHandling = fileHandling;
+        }
+
+        public void Run()
+        {
+            _fileRepository.CopyFile(_importClaimsConfiguration.Source, _importClaimsConfiguration.Destination);
+
+            var file = Path.Combine(_importClaimsConfiguration.Destination, _importClaimsConfiguration.ClaimsFile);
+            var claims = _fileHandling.ReadFile<Claim>(file);
+        }
+    }
+}
