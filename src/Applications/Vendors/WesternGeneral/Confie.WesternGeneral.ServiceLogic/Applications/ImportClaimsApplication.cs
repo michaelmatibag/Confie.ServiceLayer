e5a29dc5213f65dadb47d4a55296d43b209588c9
Michael Matibag <michaelmatibag@gmail.com>
2018-09-15 10:21:23 -0500

    Use IClaimsRepository in the application.

--- src/Applications/Vendors/WesternGeneral/Confie.WesternGeneral.ServiceLogic/Applications/ImportClaimsApplication.cs
+++ src/Applications/Vendors/WesternGeneral/Confie.WesternGeneral.ServiceLogic/Applications/ImportClaimsApplication.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.IO;
-using System.Linq;
+﻿using System.IO;
 using Confie.Infrastructure.Application;
 using Confie.Infrastructure.FileHandling;
 using Confie.Infrastructure.FileRepositories;
+using Confie.WesternGeneral.ClaimsRepository;
 using Confie.WesternGeneral.ServiceLogic.Configuration;
 
 namespace Confie.WesternGeneral.ServiceLogic.Applications
@@ -13,12 +12,14 @@ namespace Confie.WesternGeneral.ServiceLogic.Applications
         private readonly ImportClaimsConfiguration _importClaimsConfiguration;
         private readonly IFileRepository _fileRepository;
         private readonly IFileHandling _fileHandling;
+        private readonly IClaimsRepository _claimsRepository;
 
-        public ImportClaimsApplication(ImportClaimsConfiguration importClaimsConfiguration, IFileRepository fileRepository, IFileHandling fileHandling)
+        public ImportClaimsApplication(ImportClaimsConfiguration importClaimsConfiguration, IFileRepository fileRepository, IFileHandling fileHandling, IClaimsRepository claimsRepository)
         {
             _importClaimsConfiguration = importClaimsConfiguration;
             _fileRepository = fileRepository;
             _fileHandling = fileHandling;
+            _claimsRepository = claimsRepository;
         }
 
         public void Run()
@@ -27,6 +28,8 @@ namespace Confie.WesternGeneral.ServiceLogic.Applications
 
             var file = Path.Combine(_importClaimsConfiguration.Destination, _importClaimsConfiguration.ClaimsFile);
             var claims = _fileHandling.ReadFile<Claim>(file);
+
+            _claimsRepository.SaveClaims(claims);
         }
     }
 }
