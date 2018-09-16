ef34eb3638a8545c464aea19075a671528a516d1
Michael Matibag <michaelmatibag@gmail.com>
2018-09-16 07:25:58 -0500

    Add Autofac for dependency injection.

---
+++ src/Applications/Vendors/WesternGeneral/Confie.WesternGeneral.Console/DependencyResolution/ContainerConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+using Confie.Infrastructure.Application;
+using Confie.Infrastructure.FileHandling;
+using Confie.Infrastructure.FileRepositories;
+using Confie.WesternGeneral.ClaimsRepository;
+using Confie.WesternGeneral.ServiceLogic.Applications;
+using Confie.WesternGeneral.ServiceLogic.Configuration;
+
+namespace Confie.WesternGeneral.Console.DependencyResolution
+{
+    public static class ContainerConfiguration
+    {
+        public static IContainer Configure()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<ImportClaimsApplication>().As<IApplication>();
+            builder.RegisterType<ImportClaimsConfiguration>().AsSelf();
+            builder.RegisterType<FileSystemRepository>().As<IFileRepository>();
+            builder.RegisterType<FixedFileHandling>().As<IFileHandling>();
+            builder.RegisterType<ClaimsRepository.ClaimsRepository>().As<IClaimsRepository>();
+
+            return builder.Build();
+        }
+    }
+}
