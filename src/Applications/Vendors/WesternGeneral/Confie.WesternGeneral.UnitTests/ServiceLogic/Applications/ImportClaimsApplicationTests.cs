96684143dc59c475e06095114c6c5591698f3f4d
Michael Matibag <michaelmatibag@gmail.com>
2018-09-16 18:31:51 -0500

    Add unit test (expected to break).

---
+++ src/Applications/Vendors/WesternGeneral/Confie.WesternGeneral.UnitTests/ServiceLogic/Applications/ImportClaimsApplicationTests.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using Confie.Infrastructure.Application;
+using Confie.WesternGeneral.Console.DependencyResolution;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Confie.WesternGeneral.UnitTests.ServiceLogic.Applications
+{
+    [TestFixture]
+    public class ImportClaimsApplicationTests
+    {
+        [Test]
+        public void ImportClaimsApplication_Resolves()
+        {
+            var container = ContainerConfiguration.Configure();
+
+            using (var scope = container.BeginLifetimeScope())
+            {
+                var application = scope.Resolve<IApplication>();
+
+                application.ShouldNotBeNull();
+            }
+        }
+    }
+}
