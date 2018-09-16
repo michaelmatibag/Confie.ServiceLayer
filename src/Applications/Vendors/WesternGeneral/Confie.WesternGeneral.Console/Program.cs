962afc57b2ca2f758966f684f008fed7549960c6
Michael Matibag <michaelmatibag@gmail.com>
2018-09-16 07:26:39 -0500

    Tie it up together in the Main.

--- src/Applications/Vendors/WesternGeneral/Confie.WesternGeneral.Console/Program.cs
+++ src/Applications/Vendors/WesternGeneral/Confie.WesternGeneral.Console/Program.cs
@@ -1,9 +1,21 @@
-﻿namespace Confie.WesternGeneral.Console
+﻿using Autofac;
+using Confie.Infrastructure.Application;
+using Confie.WesternGeneral.Console.DependencyResolution;
+
+namespace Confie.WesternGeneral.Console
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            var container = ContainerConfiguration.Configure();
+
+            using (var scope = container.BeginLifetimeScope())
+            {
+                var application = scope.Resolve<IApplication>();
+
+                application.Run();
+            }
         }
     }
 }
