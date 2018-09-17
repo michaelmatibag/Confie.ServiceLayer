6f70b527b19eb0a951941fe59836d6bd8a36ebef
Michael Matibag <michaelmatibag@gmail.com>
2018-09-16 20:02:18 -0500

    Add more registrations.

--- src/Applications/Vendors/WesternGeneral/Confie.WesternGeneral.Console/DependencyResolution/ContainerConfiguration.cs
+++ src/Applications/Vendors/WesternGeneral/Confie.WesternGeneral.Console/DependencyResolution/ContainerConfiguration.cs
@@ -1,6 +1,8 @@
-﻿using Autofac;
+﻿using System.IO.Abstractions;
+using Autofac;
 using Confie.Infrastructure.Application;
 using Confie.Infrastructure.Configuration;
+using Confie.Infrastructure.Factories;
 using Confie.Infrastructure.FileHandling;
 using Confie.Infrastructure.FileRepositories;
 using Confie.WesternGeneral.ClaimsRepository;
@@ -21,6 +23,8 @@ namespace Confie.WesternGeneral.Console.DependencyResolution
             builder.RegisterType<FixedFileHandling>().As<IFileHandling>();
             builder.RegisterType<ClaimsRepository.ClaimsRepository>().As<IClaimsRepository>();
             builder.RegisterType<ConfigurationRepository>().As<IConfigurationRepository>();
+            builder.RegisterType<FileSystem>().As<IFileSystem>();
+            builder.RegisterType<ClaimsContextFactory>().As<IFactory<ClaimsContext>>();
 
             return builder.Build();
         }
